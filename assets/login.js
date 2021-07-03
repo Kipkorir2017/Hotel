42714db73b51e660a0cafe2c5e806254df37c79e
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-04 01:14:33 +0300

    added a login a page and js for validation

---
+++ assets/login.js
@@ -0,0 +1,18 @@
+function validate(){
+    var email=document.getElementById("form")("email").value;
+    var password =document.getElementById("form")("password").value;
+    // validating email
+    atpos=email.IndexOf("@");
+    dotpos =dotpos = email.lastIndexOf(".");
+    if(atpos <1 ||(dotpos - atpos < 2)){
+        alert("please enter a correct email");
+        return false;
+            
+        
+    }
+    // validating password
+    if(password.length <= 6 ){
+        alert("password should be more than 6 characters");
+        return false;
+    }
+}
